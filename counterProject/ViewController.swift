8b7af7c8a8dbeac48c460d68c9059907dd875f14
Dmitrii Bauzhadze <dmitry_bauzhadze@mail.ru>
2024-12-01 21:37:48 +0400

    Initial Commit

---
+++ counterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counterProject
+//
+//  Created by 0 on 1/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
