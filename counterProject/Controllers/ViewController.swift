6e87b7bcc4ec62c2379c02ea8134f6961c250ddf
DaniilOreshenkov <danieloreshenkov@yandex.ru>
2024-12-04 22:08:07 +0300

    добавлена структурность

---
+++ counterProject/Controllers/ViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: - UI Components
+    
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupLayouts()
+        setupAppearance()
+    }
+    
+    //MARK: - UI Setup
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupLayouts() {
+        
+    }
+    
+    private func setupAppearance() {
+        
+    }
+    
+    //MARK: - selector
+}
+
+#Preview(traits: .portrait) {
+    ViewController()
+}
