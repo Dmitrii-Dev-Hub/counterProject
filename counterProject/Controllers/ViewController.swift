f9f276359fa26491708491ffd77c6cb9aff70ab7
DaniilOreshenkov <danieloreshenkov@yandex.ru>
2024-12-04 23:34:14 +0300

    base viewController

--- counterProject/Controllers/ViewController.swift
+++ counterProject/Controllers/ViewController.swift
@@ -3,8 +3,6 @@ import UIKit
 class ViewController: UIViewController {
     //MARK: - UI Components
     
-    private let resetButton = ResetButton().reset
-    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +15,11 @@ class ViewController: UIViewController {
     //MARK: - UI Setup
     
     private func setupViews() {
-        resetButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(resetButton)
+        
     }
     
     private func setupLayouts() {
-        NSLayoutConstraint.activate([
-            resetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            resetButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            resetButton.widthAnchor.constraint(equalToConstant: 50),
-            resetButton.heightAnchor.constraint(equalToConstant: 50),
-        ])
+
     }
     
     private func setupAppearance() {
